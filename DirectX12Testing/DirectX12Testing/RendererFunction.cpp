861078261963cf90273de92b51a1d11954b873c4
Unknown <wwkiang@student.fullsail.edu>
2020-02-15 23:04:57 -0800

    Add render function and lib

--- DirectX12Testing/DirectX12Testing/RendererFunction.cpp
+++ DirectX12Testing/DirectX12Testing/RendererFunction.cpp
@@ -9,7 +9,6 @@
 
 using namespace Microsoft::WRL;
 
-#include "d3dx12.h"
 #include <d3dcompiler.h>
 
 #include <algorithm> // For std::min and std::max.
@@ -26,52 +25,97 @@ using namespace DirectX;
 using namespace std;
 // temp data
 
-//void RednererFunction::TransitionResource(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList, Microsoft::WRL::ComPtr<ID3D12Resource> resource,
-//	D3D12_RESOURCE_STATES beforeState, D3D12_RESOURCE_STATES afterState)
-//{
-//
-//}
-//void RednererFunction::UpdateBufferResource(
-//	ComPtr<ID3D12GraphicsCommandList2> commandList,
-//	ID3D12Resource** pDestinationResource,
-//	ID3D12Resource** pIntermediateResource,
-//	size_t numElements, size_t elementSize, const void* bufferData,
-//	D3D12_RESOURCE_FLAGS flags)
-//{
-//	/*ComPtr<ID3D12Device2> device = Application::Get().GetDevice();
-//
-//	size_t bufferSize = numElements * elementSize;
-//	// Create a committed resource for the GPU resource in a default heap.
-//	ThrowIfFailed(device->CreateCommittedResource(
-//		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-//		D3D12_HEAP_FLAG_NONE,
-//		&CD3DX12_RESOURCE_DESC::Buffer(bufferSize, flags),
-//		D3D12_RESOURCE_STATE_COPY_DEST,
-//		nullptr,
-//		IID_PPV_ARGS(pDestinationResource)));
-//	// Create an committed resource for the upload.
-//	if (bufferData)
-//	{
-//		ThrowIfFailed(device->CreateCommittedResource(
-//			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
-//			D3D12_HEAP_FLAG_NONE,
-//			&CD3DX12_RESOURCE_DESC::Buffer(bufferSize),
-//			D3D12_RESOURCE_STATE_GENERIC_READ,
-//			nullptr,
-//			IID_PPV_ARGS(pIntermediateResource)));
-//		D3D12_SUBRESOURCE_DATA subresourceData = {};
-//		subresourceData.pData = bufferData;
-//		subresourceData.RowPitch = bufferSize;
-//		subresourceData.SlicePitch = subresourceData.RowPitch;
-//
-//		UpdateSubresources(commandList.Get(),
-//			*pDestinationResource, *pIntermediateResource,
-//			0, 0, 1, &subresourceData);
-//	}*/
-//}
-//
-//void RednererFunction::ResizeDepthBuffer(int width, int height)
-//{
-//
-//}
+
+
+void RednererFunction::TransitionResource(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList, Microsoft::WRL::ComPtr<ID3D12Resource> resource,
+	D3D12_RESOURCE_STATES beforeState, D3D12_RESOURCE_STATES afterState)
+{
+
+	CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(resource.Get(), beforeState, afterState);
+
+	commandList->ResourceBarrier(1, &barrier);
+}
+void RednererFunction::ClearRTV(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList, D3D12_CPU_DESCRIPTOR_HANDLE rtv, FLOAT* clearColor)
+{
+	commandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
+}
+
+void RednererFunction::ClearDepth(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList, D3D12_CPU_DESCRIPTOR_HANDLE dsv, FLOAT depth)
+{
+}
+void RednererFunction::UpdateBufferResource(
+	ComPtr<ID3D12GraphicsCommandList2> commandList,
+	ID3D12Resource** pDestinationResource,
+	ID3D12Resource** pIntermediateResource,
+	size_t numElements, size_t elementSize, const void* bufferData,
+	D3D12_RESOURCE_FLAGS flags)
+{
+	ComPtr<ID3D12Device2> device = Application::Get().GetDevice();
+
+	size_t bufferSize = numElements * elementSize;
+	// Create a committed resource for the GPU resource in a default heap.
+	ThrowIfFailed(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(bufferSize, flags),
+		D3D12_RESOURCE_STATE_COPY_DEST,
+		nullptr,
+		IID_PPV_ARGS(pDestinationResource)));
+	// Create an committed resource for the upload.
+	if (bufferData)
+	{
+		ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Buffer(bufferSize),
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(pIntermediateResource)));
+		D3D12_SUBRESOURCE_DATA subresourceData = {};
+		subresourceData.pData = bufferData;
+		subresourceData.RowPitch = bufferSize;
+		subresourceData.SlicePitch = subresourceData.RowPitch;
+
+		UpdateSubresources(commandList.Get(),
+			*pDestinationResource, *pIntermediateResource,
+			0, 0, 1, &subresourceData);
+	}
+}
+
+void RednererFunction::ResizeDepthBuffer(int width, int height)
+{
+	if (m_ContentLoaded)
+	{
+		// Flush any GPU commands that might be referencing the depth buffer.
+		Application::Get().Flush();
+
+		width = std::max(1, width);
+		height = std::max(1, height);
+
+		auto device = Application::Get().GetDevice();
+		// Resize screen dependent resources.
+		// Create a depth buffer.
+		D3D12_CLEAR_VALUE optimizedClearValue = {};
+		optimizedClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+		optimizedClearValue.DepthStencil = { 1.0f, 0 };
+
+		ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_D32_FLOAT, width, height,
+				1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL),
+			D3D12_RESOURCE_STATE_DEPTH_WRITE,
+			&optimizedClearValue,
+			IID_PPV_ARGS(&m_DepthBuffer)
+		));        // Update the depth-stencil view.
+		D3D12_DEPTH_STENCIL_VIEW_DESC dsv = {};
+		dsv.Format = DXGI_FORMAT_D32_FLOAT;
+		dsv.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+		dsv.Texture2D.MipSlice = 0;
+		dsv.Flags = D3D12_DSV_FLAG_NONE;
+
+		device->CreateDepthStencilView(m_DepthBuffer.Get(), &dsv,
+			m_DSVHeap->GetCPUDescriptorHandleForHeapStart());
+	}
+}
 
